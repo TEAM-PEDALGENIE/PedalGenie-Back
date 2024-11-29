7ec1c1428b53b4383709bd8b51664a947175469a
gchaewon <conjs1@naver.com>
2024-11-29 21:45:59 +0900

    :tada: 프로젝트 초기 셋팅

---
+++ src/main/java/com/pedalgenie/pedalgenieback/PedalGenieBackApplication.java
@@ -0,0 +1,13 @@
+package com.pedalgenie.pedalgenieback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedalGenieBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PedalGenieBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pedalgenie/pedalgenieback/PedalGenieBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedalgenie.pedalgenieback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedalGenieBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
