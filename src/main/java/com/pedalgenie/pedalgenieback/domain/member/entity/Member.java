5558763870679bfc26422be1b7cb0a2d057c3b12
gchaewon <conjs1@naver.com>
2024-12-13 12:04:24 +0900

    :sparkles: Member 엔티티, 역할 추가

---
+++ src/main/java/com/pedalgenie/pedalgenieback/domain/member/entity/Member.java
@@ -0,0 +1,41 @@
+package com.pedalgenie.pedalgenieback.domain.member.entity;
+
+
+import com.pedalgenie.pedalgenieback.global.BaseTimeEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor (access = AccessLevel.PROTECTED)
+@Getter
+public class Member extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="member_id")
+    private Long memberId;
+
+    @NotNull
+    private String nickname;
+
+    @NotNull
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private MemberRole memberRole = MemberRole.CUSTOMER; // 디폴트는 일반 고객
+
+    private String password; // 어드민, 사장님용
+
+    @Builder
+    public Member(Long memberId, String nickname, MemberRole memberRole, String email, String password){
+        this.memberId = memberId;
+        this.nickname = nickname;
+        this.memberRole = memberRole;
+        this.email = email;
+        this.password = password;
+    }
+}
