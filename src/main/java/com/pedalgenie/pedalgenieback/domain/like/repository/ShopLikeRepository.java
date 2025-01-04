09136e4a41371fc24fd1e10b74007753cc14ea42
gchaewon <conjs1@naver.com>
2025-01-04 21:00:36 +0900

    :sparkles: 좋아요한 가게 목록 조회 API 구현

--- src/main/java/com/pedalgenie/pedalgenieback/domain/like/repository/ShopLikeRepository.java
+++ src/main/java/com/pedalgenie/pedalgenieback/domain/like/repository/ShopLikeRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -17,6 +18,11 @@ public interface ShopLikeRepository extends JpaRepository<ShopLike, Long> {
     // 가게, 멤버를 기준으로 좋아요가 존재하는지 반환하는 메서드
     boolean existsByShopAndMember(Shop shop, Member member);
     Optional<ShopLike> findByShopAndMember(Shop shop, Member member);
+
+    // 회원이 좋아요 누른 상점 리스트 반환하는 메서드
+    @Query("SELECT sl.shop FROM ShopLike sl WHERE sl.member = :member")
+    List<Shop> findLikedShopsByMember(@Param("member") Member member);
+
 
 }
 
