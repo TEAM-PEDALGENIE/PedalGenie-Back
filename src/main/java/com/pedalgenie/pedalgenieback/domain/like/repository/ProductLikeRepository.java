6ed2f1bc1ce59d5cea512216226e08d1572a4901
gchaewon <conjs1@naver.com>
2025-01-04 11:30:24 +0900

    :sparkles: 회원, 상품 기준 조회 메서드 추가

--- src/main/java/com/pedalgenie/pedalgenieback/domain/like/repository/ProductLikeRepository.java
+++ src/main/java/com/pedalgenie/pedalgenieback/domain/like/repository/ProductLikeRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface ProductLikeRepository extends JpaRepository<ProductLike, Long> {
     // productId를 기준으로 좋아요가 몇 개인지 카운트하는 메서드
     @Query("SELECT COUNT(pl) FROM ProductLike pl WHERE pl.product.id = :productId")
@@ -14,5 +16,7 @@ public interface ProductLikeRepository extends JpaRepository<ProductLike, Long> 
 
     // 상품, 멤버 기준으로 좋아요가 존재하는지 반환하는 메서드
     boolean existsByProductAndMember(Product product, Member member);
+    Optional<ProductLike> findByProductAndMember(Product product, Member member);
+
 }
 
