c85d2b3da96d2e263dd07a0ef496bd5b39ade4ef
gchaewon <conjs1@naver.com>
2025-01-06 15:57:09 +0900

    :sparkles: 좋아요 여부 필드 추가

--- src/main/java/com/pedalgenie/pedalgenieback/domain/product/dto/response/GetProductResponse.java
+++ src/main/java/com/pedalgenie/pedalgenieback/domain/product/dto/response/GetProductResponse.java
@@ -1,11 +1,13 @@
 package com.pedalgenie.pedalgenieback.domain.product.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.pedalgenie.pedalgenieback.domain.product.entity.Product;
 import com.pedalgenie.pedalgenieback.domain.productImage.application.dto.ProductImageDto;
 
 import java.util.List;
 
 // 상품 상세 조회 dto
+@JsonInclude(JsonInclude.Include.NON_NULL) // null인 필드 제외
 public record GetProductResponse(
         String name,
         String shopName,
@@ -15,10 +17,12 @@ public record GetProductResponse(
         Boolean isRentable,
         Boolean isPurchasable,
         Boolean isDemoable,
+
         // 상품 설명 텍스트, 이미지 추가
-        List<ProductImageDto> productImage
+        List<ProductImageDto> productImage,
+        Boolean isLiked
 ) {
-    public static GetProductResponse of(Product product, List<ProductImageDto> productImage){
+    public static GetProductResponse of(Product product, List<ProductImageDto> productImage, Boolean isLiked){
         return new GetProductResponse(
                 product.getName(),
                 product.getShop().getShopname(),
@@ -28,8 +32,8 @@ public record GetProductResponse(
                 product.getIsRentable(),
                 product.getIsPurchasable(),
                 product.getIsDemoable(),
-                productImage
-
+                productImage,
+                isLiked != null ? isLiked : null
         );
     }
 }
