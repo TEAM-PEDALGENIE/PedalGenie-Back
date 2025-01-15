e1107964b78575a9b4afc829b013838a79f2a93e
juanxiu <2316007@ewhain.net>
2025-01-16 01:10:15 +0900

    :sparkles: 매장 등록에 운영시간 추가

--- src/main/java/com/pedalgenie/pedalgenieback/domain/shop/dto/request/ShopCreateRequest.java
+++ src/main/java/com/pedalgenie/pedalgenieback/domain/shop/dto/request/ShopCreateRequest.java
@@ -1,33 +1,50 @@
 package com.pedalgenie.pedalgenieback.domain.shop.dto.request;
 
 import com.pedalgenie.pedalgenieback.domain.shop.entity.Shop;
+import com.pedalgenie.pedalgenieback.domain.shop.entity.ShopHours;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShopCreateRequest {
+
+    private String shopname;
+    private String address;
+    private String contactNumber;
+    private Integer demoQuantityPerDay;
+    private String detailAddress;
+    private String description;
+    private Integer instrumentCount;
+    private List<ShopHoursDto> shopHours; //운영시간 추가
 
 
-public record ShopCreateRequest(
-
-        String shopname,
-        String address,
-        String contactNumber,
-        Integer demoQuantityPerDay,
-        String businessHours,
-        String detailAddress,
-        String description,
-        Integer instrumentCount
-
-) {
-    public Shop toEntity(String url){
-        return Shop.builder()
+    public Shop toEntity(String url) {
+        Shop shop = Shop.builder()
                 .name(shopname)
                 .address(address)
                 .contactNumber(contactNumber)
                 .demoQuantityPerDay(demoQuantityPerDay)
-                .businessHours(businessHours)
                 .imageUrl(url)
                 .detailAddress(detailAddress)
                 .description(description)
                 .instrumentCount(instrumentCount)
                 .build();
 
+        List<ShopHours> shopHoursList = shopHours.stream()
+                .map(dto -> ShopHoursDto.toEntity(dto, shop))
+                .toList();
+
+        shop.addShopHours(shopHoursList);
+
+        return shop;
 
     }
 }
