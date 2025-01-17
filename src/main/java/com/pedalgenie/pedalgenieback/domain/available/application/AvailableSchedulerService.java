8b55523826ba9949a0506f16a582bae48607c01a
juanxiu <2316007@ewhain.net>
2025-01-17 23:06:41 +0900

    :rocket: 스케쥴러 테스트용

--- src/main/java/com/pedalgenie/pedalgenieback/domain/available/application/AvailableSchedulerService.java
+++ src/main/java/com/pedalgenie/pedalgenieback/domain/available/application/AvailableSchedulerService.java
@@ -15,7 +15,7 @@ public class AvailableSchedulerService {
     private final AvailableTimeService availableTimeService;
 
 //    @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul") // 매일 자정 실행
-    @Scheduled(cron = "0 05 23 * * *", zone = "Asia/Seoul") // 테스트
+    @Scheduled(cron = "0 15 23 * * *", zone = "Asia/Seoul") // 테스트
     public void resetAndGenerateAvailableTimes() {
         log.info("Scheduled task is running");
         availableTimeService.refreshAvailableTimes();
