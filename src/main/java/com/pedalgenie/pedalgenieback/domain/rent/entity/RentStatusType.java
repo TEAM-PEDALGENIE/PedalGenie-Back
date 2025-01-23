96782b7dc74fea0a0730740677c2a9d9542f70b9
juanxiu <2316007@ewhain.net>
2025-01-23 21:51:36 +0900

    :recycle: 대여 상태 필드명 수정

--- src/main/java/com/pedalgenie/pedalgenieback/domain/rent/entity/RentStatusType.java
+++ src/main/java/com/pedalgenie/pedalgenieback/domain/rent/entity/RentStatusType.java
@@ -1,11 +1,11 @@
 package com.pedalgenie.pedalgenieback.domain.rent.entity;
 
 public enum RentStatusType {
-    주문_확인_중,
-    픽업_예정,
-    사용_중,
-    반납_완료,
-    취소_접수,
-    취소_완료
+    주문확인중,
+    픽업예정,
+    사용중,
+    반납완료,
+    취소접수,
+    취소완료
 
 }
