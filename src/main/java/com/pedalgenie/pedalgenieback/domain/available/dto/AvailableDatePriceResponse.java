19caab1158496fb9981deae68e0366ad00b931e9
juanxiu <2316007@ewhain.net>
2025-01-26 14:26:52 +0900

    :recycle: 이용가능, 대여 관련 dto와 서비스에서 구매가가 아닌 일일 대여료 가져오는 것으로 변경

--- src/main/java/com/pedalgenie/pedalgenieback/domain/available/dto/AvailableDatePriceResponse.java
+++ src/main/java/com/pedalgenie/pedalgenieback/domain/available/dto/AvailableDatePriceResponse.java
@@ -3,7 +3,6 @@ package com.pedalgenie.pedalgenieback.domain.available.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.extern.java.Log;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -13,17 +12,17 @@ import java.util.List;
 @NoArgsConstructor
 public class AvailableDatePriceResponse { // 날짜들과 가격 정보
 
-    private Long price;
+    private Long rentPricePerDay;
     private Long fee; // 수수료
     private Long totalPrice; // 총금액
     private List<AvailableDateResponse> availableDates;
 
 
-    public AvailableDatePriceResponse(List<AvailableDateResponse> availableDates, BigDecimal price) {
-        BigDecimal feeAmount = price.multiply(BigDecimal.valueOf(0.1)); // 수수료 계산
-        BigDecimal totalAmount = price.add(feeAmount); // 총 금액 계산
+    public AvailableDatePriceResponse(List<AvailableDateResponse> availableDates, BigDecimal rentPricePerDay) {
+        BigDecimal feeAmount = rentPricePerDay.multiply(BigDecimal.valueOf(0.1)); // 수수료 계산
+        BigDecimal totalAmount = rentPricePerDay.add(feeAmount); // 총 금액 계산
 
-        this.price = price.longValue();
+        this.rentPricePerDay = rentPricePerDay.longValue();
         this.fee = feeAmount.longValue();
         this.totalPrice = totalAmount.longValue();
         this.availableDates = availableDates;
