a98b91c00511f0355198cf610029318204c0b6d0
juanxiu <2316007@ewhain.net>
2025-02-07 04:19:36 +0900

    :sparkles: gRPC 초기 세팅

--- src/main/java/com/pedalgenie/pedalgenieback/PedalGenieBackApplication.java
+++ src/main/java/com/pedalgenie/pedalgenieback/PedalGenieBackApplication.java
@@ -1,6 +1,7 @@
 package com.pedalgenie.pedalgenieback;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -8,6 +9,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableScheduling
+@EnableAutoConfiguration(exclude = {
+        net.devh.boot.grpc.server.autoconfigure.GrpcServerSecurityAutoConfiguration.class
+}) // gRPC 보안 관련 자동 설정 제외
 public class PedalGenieBackApplication {
 
     public static void main(String[] args) {
